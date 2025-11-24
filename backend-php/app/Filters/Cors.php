559c66b76bec9f8b97343b7d8c746210cb151c14
Eric Moore <emoore@thunkit.biz>
2025-11-24 10:24:25 -0600

    master: restrict CORS allow origin in after function.

--- backend-php/app/Filters/Cors.php
+++ backend-php/app/Filters/Cors.php
@@ -27,7 +27,7 @@ class Cors implements FilterInterface
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
     {
         // Repeat headers for actual response
-        $response->setHeader('Access-Control-Allow-Origin', '*');
+        $response->setHeader('Access-Control-Allow-Origin', env('ALLOWED_ORIGINS'));
         $response->setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
         $response->setHeader('Access-Control-Allow-Credentials', 'true');
